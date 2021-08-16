6320abbbc99cbaf7fc098643fd0a464083c30149
sqrlfirst <egor.dergunov1996@gmail.com>
2021-08-16 19:23:50 +0700

    add swap struct, @Sabomanq61 check

--- contracts/bridge.sol
+++ contracts/bridge.sol
@@ -3,13 +3,19 @@ pragma solidity >=0.8.4;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-
 contract bridge is AccessControl {
 
     bytes32 public constant VALIDATOR_ROLE = keccak256("VALIDATOR_ROLE");
+    enum STATE {UNDONE, WAIT, DONE} // states writen as an example 
+                                    // not working one, change later
+
+    struct SwapsInfo{
+        STATE state;
+        uint256 nonce; // not sure about type 
+    };
 
     mapping (string => address) tokensBySymbol;
-    mapping () swaps;       // CONTINUE
+    mapping (bytes32 => SwapsInfo) swaps;       // CONTINUE
 
     event swapHappend();
 
@@ -31,9 +37,9 @@ contract bridge is AccessControl {
         emit swapHappend();
     }
 
-    function addToken {
+    function addToken(string _tokenSymbol, address _tokenAdress) external {
         // add tokens to contract for  swapping // 
-        
+
     }
 
 }
