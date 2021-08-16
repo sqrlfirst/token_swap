fd59356d347be38278cd54f34ef6327b9ccb086e
sqrlfirst <egor.dergunov1996@gmail.com>
2021-08-16 19:03:23 +0700

    write amth in bridge

--- contracts/bridge.sol
+++ contracts/bridge.sol
@@ -1,6 +1,39 @@
 //"SPDX-License-Identifier: MIT"
 pragma solidity >=0.8.4;
 
-contract bridge {
-    
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+
+contract bridge is AccessControl {
+
+    bytes32 public constant VALIDATOR_ROLE = keccak256("VALIDATOR_ROLE");
+
+    mapping (string => address) tokensBySymbol;
+    mapping () swaps;       // CONTINUE
+
+    event swapHappend();
+
+    constructor (
+        address addr_back
+    ) public {
+        _setupRole(VALIDATOR_ROLE, addr_back);      // _??_: it's correct way to set up validator role?//  
+
+    }
+
+
+    function swap {
+        /*  - burn tokens from user                 *
+         *  - write to swap MAP hash of transaction *
+         *  - change status of swap                 *
+         *  - emit swapHappend event                */ 
+
+
+        emit swapHappend();
+    }
+
+    function addToken {
+        // add tokens to contract for  swapping // 
+        
+    }
+
 }
