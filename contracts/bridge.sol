4163f252fb1268fa94cdc82c481111206ffdf072
sqrlfirst <egor.dergunov1996@gmail.com>
2021-08-18 11:34:39 +0700

    import some additional libs

--- contracts/bridge.sol
+++ contracts/bridge.sol
@@ -2,7 +2,11 @@
 pragma solidity >=0.8.4;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "./bulldogtoken.sol";
 
 
